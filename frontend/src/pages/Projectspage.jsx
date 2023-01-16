66ffe06d4967b7e3404feb51d86f4d01142caa64
almalmalm <almostkalinin@gmail.com>
2023-01-16 21:42:42 +0400

    FIX:layout on page projects

--- frontend/src/pages/Projectspage.jsx
+++ frontend/src/pages/Projectspage.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Projects from '../components/Projects';
-import TextSphere from '../components/TextSphere';
 
 const Projectspage = () => {
   return (
-    <div className="w-screen mx-auto h-screen">
+    <div className="w-screen mx-auto h-screen flex flex-col justify-between">
       <Header />
-      <div className="sm:flex justify-between text-sm mt-2 mx-4 sm:text-base sm:mt-4 sm:mx-8 lg:mx-16">
+      <div className="sm:flex justify-between text-sm mt-2 mx-4 sm:text-base sm:mt-16 sm:mx-8 lg:mx-16 grow">
         <Projects />
-        <TextSphere />
       </div>
       <Footer />
     </div>
